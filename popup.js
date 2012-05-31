f30ac34159c2b7a40ac4d9fa903dea80714397a1
Aimee L <aimee@cheezburger.com>
2012-05-31 12:37:06 -0700

    daily word now updates from appharbor

--- popup.js
+++ popup.js
@@ -1,10 +1,19 @@
-var dailyWord = "Globular";
-var searchin = $('*:contains("'+ dailyWord +'")');
 
-function show(dailyWord){
+var callbackSuccess = function(data) {
+	var dailyWord = data;
+  	startLookin(dailyWord);
+};
+
+var startLookin = function(dailyWord){
+	var searchin = $('*:contains("'+ dailyWord +'")');
+
+	if( searchin.length >= 1){
+		show(dailyWord);
+	};
+};
+
+var show = function(dailyWord){
 	chrome.extension.sendRequest(dailyWord);
 };
 
-if( searchin.length >= 1){
-	show(dailyWord);
-};
+$.get('http://conky.apphb.com', callbackSuccess);
